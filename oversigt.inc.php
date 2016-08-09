d6755c89e5c64bda2087f258d555559255a1b092
Claus Tøndering <claus@ezer.dk>
2016-08-09 15:15:04 +0200

    Es 2,3,4,5,8 tilføjet

--- oversigt.inc.php
+++ oversigt.inc.php
@@ -3,7 +3,7 @@
 $chap['dom']      = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21);
 $chap['ruth']     = array(1, 2, 3, 4);
 $chap['sl']       = array(1, 2, 8, 23, 24);
-$chap['es']       = array(1, 6, 7);
+$chap['es']       = array(1, 2, 3, 4, 5, 6, 7, 8);
 $chap['jer']      = array(1, 2, 7);
 
 $title['dom']     = 'Dommerbogen';
