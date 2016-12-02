89b911d57b216ce1c34c46ea01807ca68bd3bc94
Claus Tøndering <claus@ezer.dk>
2016-12-02 16:02:59 +0100

    Trykfejl rettet

--- andreformater.php
+++ andreformater.php
@@ -16,7 +16,7 @@ makemenus(4);
             <div class="panel-body">
               <h1>Andre formater</h1>
               
-              <p>En PDF-fil med de bibeltekster der er markeret »færdig« eller »delvis færdig« kan downloades <a href="dfb.pdf">her</a>.</p>
+              <p>En PDF-fil med de bibeltekster der er markeret »færdig« eller »delvis færdig«, kan downloades <a href="dfb.pdf">her</a>.</p>
 
               <p>Dommerbogen findes som lydbog, indtalt af Nicklas Lautrup-Meiner.
               Bogen kan hentes gratis hos <a href="https://www.saxo.com/dk/dommerbogen-den-frie-bibel_lydbog-download_9788771960266" target="_blank">Saxo</a>.</p>
