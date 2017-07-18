2b0805434de21cc4779070fae4a196d7dc7c73df
Claus Tøndering <claus@ezer.dk>
2017-07-18 14:00:05 +0200

    På forside tilføjet 4. Mosebog

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ makemenus(0);
             engelske <a href="http://ebible.org/web/copyright.htm" target="_blank">World English
             Bible</a>.</p>
 
-          <p>Af Det Gamle Testamente foreligger Dommerbogen, Ruths Bog, Salmernes bog, Zakarias&rsquo; Bog og nogle få kapitler af andre
+          <p>Af Det Gamle Testamente foreligger 4. Mosebog, Dommerbogen, Ruths Bog, Salmernes bog, Zakarias&rsquo; Bog og nogle få kapitler af andre
             bøger. Forlægget har
             været den danske oversættelse fra 1871, som er blevet moderniseret og opdateret med nye
             resultater fra bibelforskningen. (Nærværende udgave af Dommerbogen er udarbejdet i
