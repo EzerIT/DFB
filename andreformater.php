4e73f226258b100eb5954ee1eabd3866d86ce46c
Claus Tøndering <claus@tondering.dk>
2026-01-27 15:13:47 +0100

    Bedre tekst i andreformater.php

--- andreformater.php
+++ andreformater.php
@@ -16,7 +16,7 @@ makemenus(4);
               <img class="img-fluid float-right d-none d-lg-block" style="width: 220px; margin-top: 0px; margin-left: 10px" src="img/pexels-cup-of-couple-6177682.jpg" alt="">
               <h1 class="card-title">Andre formater</h1>
               
-              <p class="card-text">En PDF-fil med de bibeltekster der er markeret »færdig« eller »delvis færdig«, kan downloades <a href="dfb.pdf">her</a>.</p>
+              <p class="card-text">En PDF-fil med de bibeltekster der ikke er markeret med »ufuldstændigt«, kan downloades <a href="dfb.pdf">her</a>.</p>
             </div>
           </div>
         </div>
