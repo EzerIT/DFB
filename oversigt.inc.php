64566fb26bf75da957fea8cd8991cdae886a9a12
Claus Tøndering <claus@tondering.dk>
2026-02-11 14:31:38 +0100

    Rettelser til 2 Mos 19 - oversigt

--- oversigt.inc.php
+++ oversigt.inc.php
@@ -228,6 +228,7 @@ for ($k=15; $k<=50; ++$k)
 for ($k=1; $k<=40; ++$k)
     $style['2mos'][$k] = $modenhed['uden indrykning'];
 $style['2mos'][14] = $modenhed['med indrykning'];
+$style['2mos'][19] = $modenhed['med indrykning'];
 
 $style['3mos']    = $modenhed['uden indrykning'];
 
