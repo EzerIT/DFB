0a4b44837d4cfceac4b0ab0dbb6ea76137060b19
Claus Tøndering <claus@tondering.dk>
2026-03-02 14:32:51 +0100

    Rettelser i 2 Mos 32

--- oversigt.inc.php
+++ oversigt.inc.php
@@ -230,6 +230,7 @@ for ($k=1; $k<=40; ++$k)
 $style['2mos'][14] = $modenhed['med indrykning'];
 $style['2mos'][19] = $modenhed['med indrykning'];
 $style['2mos'][20] = $modenhed['med indrykning'];
+$style['2mos'][32] = $modenhed['med indrykning'];
 
 $style['3mos']    = $modenhed['uden indrykning'];
 
